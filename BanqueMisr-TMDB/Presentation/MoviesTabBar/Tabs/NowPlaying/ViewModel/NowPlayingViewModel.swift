84610cbe0e1b45882fd57b803a0608b3622cf9ad
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-26 20:38:51 +0200

    Tailored the NowPlayingViewModelInputs protocol
    Tailored the NowPlayingViewModelInputs protocol to fit app design

--- BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/ViewModel/NowPlayingViewModel.swift
+++ BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/ViewModel/NowPlayingViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import Combine
 
 protocol NowPlayingViewModelInputs {
-  func viewDidLoad()
-  func didLoadNextPage()
+  func fetchMoviesList(loading:MoviesListViewModelLoading) async throws -> [NowPlayingListItemViewModel]
+  func didLoadNextPage() async throws -> [NowPlayingListItemViewModel]
   func didSelectItem(at index: Int)
 }
 
@@ -21,11 +21,9 @@ enum MoviesListViewModelLoading {
 
 protocol NowPlayingViewModelOutputs {
   var loading: PassthroughSubject<MoviesListViewModelLoading?,Never> { get }
-  var error: PassthroughSubject<String,Never> { get }
   var isEmpty: Bool { get }
   var screenTitle: String { get }
   var emptyDataTitle: String { get }
-  var errorTitle: String { get }
 }
 
 typealias NowPlayingViewModel = NowPlayingViewModelInputs & NowPlayingViewModelOutputs
@@ -34,36 +32,57 @@ final class DefaultNowPlayingViewModel:NowPlayingViewModel {
   //MARK: - Properties
   private let moviesListUseCase:MoviesListUseCase
   private var pages :[MoviesPage] = []
+  private var isCurrentlyFetching:Bool = false
   var currentPage:Int = 0
   var totalPageCount:Int = 1
   var hasMorePages:Bool { currentPage < totalPageCount }
   var nextPage: Int { hasMorePages ? currentPage + 1 : currentPage }
   //MARK: - Outputs
   var loading: PassthroughSubject<MoviesListViewModelLoading?, Never> = .init()
-  var error: PassthroughSubject<String, Never> = .init()
-  var isEmpty: Bool { return true }
+  var isEmpty: Bool { pages.movies.isEmpty }
   var screenTitle: String { "Now Playing"}
   var emptyDataTitle: String { "Couldn't load Now Playing movies"}
-  var errorTitle: String { "Error" }
   //MARK: - Init
   init(moviesListUseCase:MoviesListUseCase) {
     self.moviesListUseCase = moviesListUseCase
   }
-  //MARK: - Methods
+  //MARK: - Private methods
+  private func appendPage(_ moviesPage: MoviesPage) {
+    currentPage = moviesPage.page
+    totalPageCount = moviesPage.totalPages
+    
+    pages = pages
+      .filter { $0.page != moviesPage.page }
+    + [moviesPage]
+  }
 }
 //MARK: - Inputs
 extension DefaultNowPlayingViewModel {
   
-  func viewDidLoad() {
-    
+  func fetchMoviesList(loading:MoviesListViewModelLoading) async throws -> [NowPlayingListItemViewModel] {
+    self.loading.send(loading)
+    self.isCurrentlyFetching = true
+    let moviesList = try await moviesListUseCase.execute(for: .nowPlaying,
+                                                         requestValue: .init(page: nextPage))
+    self.appendPage(moviesList)
+    self.loading.send(.none)
+    self.isCurrentlyFetching = false
+    return pages.movies.map(NowPlayingListItemViewModel.init)
   }
   
-  func didLoadNextPage() {
-    
+  func didLoadNextPage() async throws -> [NowPlayingListItemViewModel] {
+    guard hasMorePages, !isCurrentlyFetching else {
+      return pages.movies.map(NowPlayingListItemViewModel.init)
+    }
+    return try await fetchMoviesList(loading: .nextPage)
   }
   
   func didSelectItem(at index: Int) {
     
   }
   
+}
+//MARK: - Private extension
+private extension Array where Element == MoviesPage {
+  var movies: [Movie] { flatMap { $0.movies } }
 }
