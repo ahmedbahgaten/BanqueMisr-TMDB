79637a0b522d1f6f1e591af6b1576831bc1cd1ce
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-26 19:36:00 +0200

    Added some new data to the DTOs and Entities
    Added some new data to the DTOs and Entities to be used later in displaying movie details

--- BanqueMisr-TMDB/Data/DTOs/MovieResponseDTO.swift
+++ BanqueMisr-TMDB/Data/DTOs/MovieResponseDTO.swift
@@ -23,12 +23,20 @@ extension MoviesResponseDTO {
     private enum CodingKeys: String, CodingKey {
       case id
       case title
+      case genre
       case posterPath = "poster_path"
+      case overview
       case releaseDate = "release_date"
+    }
+    enum GenreDTO: String, Decodable {
+      case adventure
+      case scienceFiction = "science_fiction"
     }
     let id: Int
     let title: String?
+    let genre: GenreDTO?
     let posterPath: String?
+    let overview: String?
     let releaseDate: String?
   }
 }
@@ -47,7 +55,18 @@ extension MoviesResponseDTO.MovieDTO {
   func toDomain() -> Movie {
     return .init(id: Movie.Identifier(id),
                  title: title,
+                 genre: genre?.toDomain(),
                  posterPath: posterPath,
+                 overview: overview,
                  releaseDate: releaseDate?.toDate)
+  }
+}
+
+extension MoviesResponseDTO.MovieDTO.GenreDTO {
+  func toDomain() -> Movie.Genre {
+    switch self {
+      case .adventure: return .adventure
+      case .scienceFiction: return .scienceFiction
+    }
   }
 }
