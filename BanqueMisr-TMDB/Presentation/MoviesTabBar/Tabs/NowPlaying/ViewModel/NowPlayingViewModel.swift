f98a85ac5c919be5738e89fa21ea9e7ca170b623
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-26 18:53:51 +0200

    Implemented Inputs and outputs protocols
    Implemented Inputs and outputs protocols for DefaultNowPlayingViewModel

--- BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/ViewModel/NowPlayingViewModel.swift
+++ BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/ViewModel/NowPlayingViewModel.swift
@@ -30,13 +30,40 @@ protocol NowPlayingViewModelOutputs {
 
 typealias NowPlayingViewModel = NowPlayingViewModelInputs & NowPlayingViewModelOutputs
 
-final class DefaultNowPlayingViewModel {
+final class DefaultNowPlayingViewModel:NowPlayingViewModel {
   //MARK: - Properties
   private let moviesListUseCase:MoviesListUseCase
+  private var pages :[MoviesPage] = []
+  var currentPage:Int = 0
+  var totalPageCount:Int = 1
+  var hasMorePages:Bool { currentPage < totalPageCount }
+  var nextPage: Int { hasMorePages ? currentPage + 1 : currentPage }
+  //MARK: - Outputs
+  var loading: PassthroughSubject<MoviesListViewModelLoading?, Never> = .init()
+  var error: PassthroughSubject<String, Never> = .init()
+  var isEmpty: Bool { return true }
+  var screenTitle: String { "Now Playing"}
+  var emptyDataTitle: String { "Couldn't load Now Playing movies"}
+  var errorTitle: String { "Error" }
   //MARK: - Init
   init(moviesListUseCase:MoviesListUseCase) {
     self.moviesListUseCase = moviesListUseCase
   }
   //MARK: - Methods
+}
+//MARK: - Inputs
+extension DefaultNowPlayingViewModel {
+  
+  func viewDidLoad() {
+    
+  }
+  
+  func didLoadNextPage() {
+    
+  }
+  
+  func didSelectItem(at index: Int) {
+    
+  }
   
 }
