619cee633e5ec01c46d0d35ed9460776483b1a13
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-28 00:40:09 +0200

    Refactored the TabbarDIContainer
    Refactoring the TabbarDIContainer to accommodate the introduction of coordinators in the app

--- BanqueMisr-TMDB/DIContainer/MoviesTabBarDIContainer.swift
+++ BanqueMisr-TMDB/DIContainer/MoviesTabBarDIContainer.swift
@@ -9,9 +9,9 @@ import Foundation
 import UIKit
 
 protocol MoviesTabBarFlowCoordinatorDependencies {
-  func makeNowPlayingMoviesViewController() -> UINavigationController
-  func makePopularMoviesViewController() -> UINavigationController
-  func makeUpcomingMoviesViewController() -> UINavigationController
+  func makeNowPlayingMoviesCoordinator() -> Coordinator
+  func makePopularMoviesCoordinator() -> Coordinator
+  func makeUpcomingMoviesCoordinator() -> Coordinator
 }
 
 final class MoviesTabBarDIContainer {
@@ -28,49 +28,35 @@ final class MoviesTabBarDIContainer {
     self.dependencies = dependencies
   }
     // MARK: - Flow Coordinators
-  func makeAppTabBarController() -> AppTabBarController {
-    let nowPlayingMoviesVC = makeNowPlayingMoviesViewController()
-    let popularMoviesVC = makePopularMoviesViewController()
-    let upcomingMoviesVC = makeUpcomingMoviesViewController()
-    let tabBarController = AppTabBarController(viewControllers: [nowPlayingMoviesVC,popularMoviesVC,upcomingMoviesVC])
-    return tabBarController
-  }
-  
-  private func setupTabBarView(controller:UIViewController,
-                               title:String,
-                               imageName:String) {
-    controller.tabBarItem.image = UIImage(systemName: imageName)
-    controller.tabBarItem.title = title
+  func makeTabBarCoordinators() -> [Coordinator] {
+    let nowPlayingCoordinator = makeNowPlayingMoviesCoordinator()
+    let popularCoordinator = makePopularMoviesCoordinator()
+    let upcomingCoordinator = makeUpcomingMoviesCoordinator()
+    return [nowPlayingCoordinator,popularCoordinator,upcomingCoordinator]
   }
 }
 
 extension MoviesTabBarDIContainer:MoviesTabBarFlowCoordinatorDependencies {
   
-  func makeNowPlayingMoviesViewController() -> UINavigationController {
+  func makeNowPlayingMoviesCoordinator() -> Coordinator {
     let tableViewController = makeMoviesListTableViewController(for: .nowPlaying)
-    let nowPlayingVC = NowPlayingViewController(moviesListTableViewController: tableViewController)
-    setupTabBarView(controller: nowPlayingVC,
-                    title: "Now Playing",
-                    imageName: "play.circle")
-    return UINavigationController(rootViewController: nowPlayingVC)
+    let coordinator = NowPlayingCoordinator(navigationController: UINavigationController(),
+                                            tableViewController: tableViewController)
+    return coordinator
   }
   
-  func makePopularMoviesViewController() -> UINavigationController {
+  func makePopularMoviesCoordinator() -> Coordinator {
     let tableViewController = makeMoviesListTableViewController(for: .popular)
-    let popularMoviesVC = PopularMoviesViewController(moviesListTableViewController: tableViewController)
-    setupTabBarView(controller: popularMoviesVC,
-                    title: "Popular",
-                    imageName: "chart.line.uptrend.xyaxis.circle")
-    return UINavigationController(rootViewController: popularMoviesVC)
+    let coordinator = PopularCoordinator(navigationController: UINavigationController(),
+                                            tableViewController: tableViewController)
+    return coordinator
   }
   
-  func makeUpcomingMoviesViewController() -> UINavigationController {
+  func makeUpcomingMoviesCoordinator() -> Coordinator {
     let tableViewController = makeMoviesListTableViewController(for: .upcoming)
-    let upcomingMoviesVC = UpcomingMoviesViewController(moviesListTableViewController: tableViewController)
-    setupTabBarView(controller: upcomingMoviesVC,
-                    title: "Upcoming",
-                    imageName: "arrow.down.circle.dotted")
-    return UINavigationController(rootViewController: upcomingMoviesVC)
+    let coordinator = UpcomingCoordinator(navigationController: UINavigationController(),
+                                            tableViewController: tableViewController)
+    return coordinator
   }
 }
 extension MoviesTabBarDIContainer {
