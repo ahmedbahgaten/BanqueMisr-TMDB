1ffa0b01f41d5ef76b5f4f3741eb37de2377ae95
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-28 15:14:00 +0200

    Added the initial design of MovieDetails screen
    Added the initial design of MovieDetails screen and rendered the movieDetails

--- BanqueMisr-TMDB/Presentation/MovieDetails/ViewController/MovieDetailsViewController.swift
+++ BanqueMisr-TMDB/Presentation/MovieDetails/ViewController/MovieDetailsViewController.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  @IBOutlet private weak var spokenLanguageLbl: UILabel!
+  @IBOutlet private weak var moviePosterImgView: UIImageView!
+  @IBOutlet private weak var stackView: UIStackView!
+  @IBOutlet private weak var overviewLbl: UILabel!
+  @IBOutlet private weak var runtimeLbl: UILabel!
+  @IBOutlet private weak var budgetLbl: UILabel!
+  @IBOutlet private weak var statusLbl: UILabel!
+  @IBOutlet private weak var genresLbl: UILabel!
+  
+  private let viewModel:MovieDetailsViewModel
+  
+  init(viewModel: MovieDetailsViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupView()
+    loadMovieDetails()
+  }
+  
+  private func setupView() {
+    moviePosterImgView.layer.cornerRadius = 8
+  }
+  
+  private func loadMovieDetails() {
+    Task {
+      do {
+        let details = try await viewModel.fetchMovieDetails()
+        renderMovieDetailsUI(movieDetails: details)
+      }catch {
+        
+      }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  private func renderMovieDetailsUI(movieDetails:MovieDetails) {
+    stackView.isHidden = false
+    moviePosterImgView.isHidden = false
+    overviewLbl.text = movieDetails.overview
+    moviePosterImgView.image = ImageCacheManager.shared.getImage(forKey: movieDetails.posterPath)
+    genresLbl.text = movieDetails.genresTxt
+    runtimeLbl.text = movieDetails.runtime.description
+    budgetLbl.text = movieDetails.budget.description + "$"
+    statusLbl.text = movieDetails.status
+    spokenLanguageLbl.text = movieDetails.spokenLangaugesTxt
+  }
 }
--- BanqueMisr-TMDB/Presentation/MovieDetails/ViewModel/MovieDetailsViewModel.swift
+++ BanqueMisr-TMDB/Presentation/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+
+protocol MovieDetailsViewModelInputs {
+  func fetchMovieDetails() async throws -> MovieDetails
+}
+
+protocol MovieDetailsViewModelOutputs {
+  var movieDetails:MovieDetails? { get }
+}
+
+typealias MovieDetailsViewModel = MovieDetailsViewModelInputs & MovieDetailsViewModelOutputs
+
+final class DefaultMovieDetailsViewModel {
+  //MARK: - Properties
+  private let movieDetailsUseCase:MovieDetailsUseCase
+  private let imgFetchingRepo:FetchImageRepository
+  private let movieID:String
+  var movieDetails: MovieDetails?
+  
+  init(movieDetailsUseCase: MovieDetailsUseCase,
+       imgFetchingRepo: FetchImageRepository,
+       movieID:String) {
+    self.movieDetailsUseCase = movieDetailsUseCase
+    self.imgFetchingRepo = imgFetchingRepo
+    self.movieID = movieID
+  }
+}
+extension DefaultMovieDetailsViewModel:MovieDetailsViewModel {
+  func fetchMovieDetails() async throws -> MovieDetails {
+    return try await movieDetailsUseCase.execute(for: self.movieID)
+  }
+}
