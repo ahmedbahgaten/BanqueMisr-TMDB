b69d90adb8c424732cfc4e260d0daa7d256d450d
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-29 01:30:59 +0200

    Initial setup for MovieDetails tests
    Initial setup for MovieDetails tests implementation

--- BanqueMisr-TMDBTests/Presentation/Mocks/MovieDetailsUseCaseMock.swift
+++ BanqueMisr-TMDBTests/Presentation/Mocks/MovieDetailsUseCaseMock.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+@testable import BanqueMisr_TMDB
+
+final class MovieDetailsUseCaseMock {
+  
+  var callCount = 0
+  var movieDetails: MovieDetails?
+  var error:NetworkError?
+  
+}
+extension MovieDetailsUseCaseMock:MovieDetailsUseCase {
+  func execute(for movieID: String) async throws -> MovieDetails {
+    callCount += 1
+    if let error = error {
+      throw error
+    }
+    return movieDetails
+  }
+}
