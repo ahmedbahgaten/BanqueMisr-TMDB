6ff16c4705daa9cbac5e7cfde226b4da4c323a74
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-28 15:15:27 +0200

    Navigating to MovieDetails from the coordinator
    Navigating to MovieDetails from the NowPlayingCoordinator

--- BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/Coordinator/NowPlayingCoordinator.swift
+++ BanqueMisr-TMDB/Presentation/MoviesTabBar/Tabs/NowPlaying/Coordinator/NowPlayingCoordinator.swift
@@ -30,7 +30,14 @@ final class NowPlayingCoordinator:Coordinator {
   func finish() { }
   
   func navigateToMovieDetails(with id:String) {
-    let movieDetailsVC = MovieDetailsViewController()
+    let diContainer = AppDIContainer()
+    let movieDetailsRepo = DefaultMovieDetailsRepository(dataTransferService: diContainer.apiDataTransferService)
+    let imgFetchingRepo = DefaultFetchImageRepository(dataTransferService: diContainer.imageDataTransferService)
+    let movieDetailsUseCase = DefaultMovieDetailstUseCase(movieDetailsRepository: movieDetailsRepo)
+    let viewModel = DefaultMovieDetailsViewModel(movieDetailsUseCase: movieDetailsUseCase,
+                                                 imgFetchingRepo: imgFetchingRepo,
+                                                 movieID: id)
+    let movieDetailsVC = MovieDetailsViewController(viewModel: viewModel)
     navigationController.pushViewController(movieDetailsVC, animated: true)
   }
   
