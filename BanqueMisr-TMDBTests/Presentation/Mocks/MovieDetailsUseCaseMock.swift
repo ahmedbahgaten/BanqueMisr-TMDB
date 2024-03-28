68b5635c9e2648937009f3a0a86af7c39c4d45eb
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-29 01:29:07 +0200

    Created MovieDetails Tests requirements

---
+++ BanqueMisr-TMDBTests/Presentation/Mocks/MovieDetailsUseCaseMock.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailsUseCaseMock.swift
+//  BanqueMisr-TMDBTests
+//
+//  Created by Ahmed Bahgat on 29/03/2024.
+//
+
+import Foundation
---
+++ BanqueMisr-TMDBTests/Presentation/Mocks/MoviesListUseCaseMock.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesListUseCaseMock.swift
+//  BanqueMisr-TMDBTests
+//
+//  Created by Ahmed Bahgat on 28/03/2024.
+//
+
+import Foundation
