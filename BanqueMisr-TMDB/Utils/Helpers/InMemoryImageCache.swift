d281540398ca452a6b7b0d145e6eb7d1ba6bcc15
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-30 00:50:38 +0200

    Changed the InMemoryImageCache
    Changed the InMemoryImageCache to let it depend on NSCache

--- BanqueMisr-TMDB/Utils/Helpers/InMemoryImageCache.swift
+++ BanqueMisr-TMDB/Utils/Helpers/InMemoryImageCache.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class InMemoryImageCache {
   static let shared = InMemoryImageCache()
-  private var cache:[String:Data] = [:]
-  
-  private init() {}
+  private let cache = NSCache<NSString, NSData>()
+
+  private init() {
+    cache.countLimit = 0
+    cache.totalCostLimit = 0
+    
+  }
   
   func getImage(forKey key: String) -> Data? {
-    return cache[key]
+    if let cachedData = cache.object(forKey: key as NSString) {
+      return cachedData as Data
+    }
+    return nil
   }
   
   func setImage(_ image: Data, forKey key: String) {
-    cache[key] = image
+    cache.setObject(image as NSData, forKey: key as NSString)
   }
 }
