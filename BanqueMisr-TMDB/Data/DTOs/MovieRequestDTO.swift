61f378cb6f46fd5b7888ad915f7dc693583c3f4f
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-30 00:26:01 +0200

    Added extension method on MoviesRequestDTO
    Added extension method on MoviesRequestDTO to be able to work with CoreData entities

--- BanqueMisr-TMDB/Data/DTOs/MovieRequestDTO.swift
+++ BanqueMisr-TMDB/Data/DTOs/MovieRequestDTO.swift
@@ -6,6 +6,15 @@
 //
 
 import Foundation
+import CoreData
 struct MoviesRequestDTO: Encodable {
   let page: Int
+}
+
+extension MoviesRequestDTO {
+  func toEntity(in context: NSManagedObjectContext) -> MoviesListRequestEntity {
+    let entity: MoviesListRequestEntity = .init(context: context)
+    entity.page = Int32(page)
+    return entity
+  }
 }
