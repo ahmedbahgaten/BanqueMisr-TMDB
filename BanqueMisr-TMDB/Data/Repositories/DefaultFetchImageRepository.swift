3316302ea89b600a5fd44411de70af9946713a4c
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-03-29 21:55:50 +0200

    Refactoring DefaultFetchImageRepo
    Refactoring DefaultFetchImageRepo to be able to fetch the image from local storage when the app is offline

--- BanqueMisr-TMDB/Data/Repositories/DefaultFetchImageRepository.swift
+++ BanqueMisr-TMDB/Data/Repositories/DefaultFetchImageRepository.swift
@@ -8,17 +8,34 @@
 import Foundation
 final class DefaultFetchImageRepository {
   
-  private let dataTransferService: DataTransferService  
+  private let dataTransferService: DataTransferService
+  private let localStorage:MoviePosterImageLocalStorage
   init(
-    dataTransferService: DataTransferService) {
-    self.dataTransferService = dataTransferService
+    dataTransferService: DataTransferService,
+    localStorage:MoviePosterImageLocalStorage) {
+      self.dataTransferService = dataTransferService
+      self.localStorage = localStorage
+    }
+  
+  private func saveImage(data:Data, for imagePath:String) async throws {
+    InMemoryImageCache.shared.setImage(data, forKey: imagePath)
+    try await localStorage.saveImage(for: imagePath, data: data)
   }
 }
 
 extension DefaultFetchImageRepository: FetchImageRepository {
   
   func fetchImage(with imagePath: String, width: Int) async throws -> Data {
-    let endpoint = APIEndpoints.getMoviePoster(path: imagePath, width: width)
-    return try await dataTransferService.request(with: endpoint)
+    if let inMemoryCachedImage = InMemoryImageCache.shared.getImage(forKey: imagePath) {
+      return inMemoryCachedImage
+    }else if let coreDataCachedImage = try await localStorage.getImage(for: imagePath) {
+      return coreDataCachedImage
+    }else {
+      let endpoint = APIEndpoints.getMoviePoster(path: imagePath, width: width)
+      let imageData = try await dataTransferService.request(with: endpoint)
+      try await saveImage(data: imageData,
+                          for: imagePath)
+      return imageData
+    }
   }
 }
